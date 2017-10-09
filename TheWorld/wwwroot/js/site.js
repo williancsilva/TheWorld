40fbaec252bc2aa9bfc329433713ed42c2bf3f13
Willian Candido <williancsilva@gmail.com>
2017-10-09 20:02:36 -0300

    first commit

---
+++ TheWorld/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿//site.js
+(function () {
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Zoro";
+
+    var main = document.getElementById("main");
+
+    main.onmouseenter = function () {
+        main.style.backgroundColor = "#888";
+    };
+
+    main.onmouseleave = function () {
+        main.style.backgroundColor = "";
+    };
+})();
